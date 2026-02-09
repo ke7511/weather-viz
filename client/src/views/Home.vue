5bcd7b54ad4a161130f1ef6ab76455387f043552
soap <149249217+ke7511@users.noreply.github.com>
2026-02-09 17:01:47 +0800

    refactor(Home): 封装获取天气数据的 组合式函数

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { useIntervalFn } from '@vueuse/core'
-import { storeToRefs } from 'pinia'
 import CitySearch from '@/modules/home/CitySearch.vue'
 import LocationBadge from '@/modules/home/LocationBadge.vue'
 import Icon from '@/components/Icon.vue'
@@ -10,90 +7,27 @@ import SkeletonWeatherCard from '@/components/skeleton/SkeletonWeatherCard.vue'
 import SkeletonIndicators from '@/components/skeleton/SkeletonIndicators.vue'
 import SkeletonChart from '@/components/skeleton/SkeletonChart.vue'
 import SkeletonForecast from '@/components/skeleton/SkeletonForecast.vue'
-import type { CityInfo } from '@/api/city'
-import { getAirQualityApi, type AQIInfo, type PollutantInfo } from '@/api/air'
-import {
-  getWeatherApi,
-  getSunriseSunsetApi,
-  type weatherInfo,
-  type HourlyWeatherInfo,
-  getHourlyWeatherApi
-} from '@/api/weather'
-import { getUVIndexApi, type UVIndexInfo } from '@/api/indices'
-import { useLocationStore } from '@/stores/location'
 import {
   AirQuality,
   TemperatureTrend,
   DailyForecast,
   IndicatorsGrid
 } from '@/utils/lazyComponents'
+import { useWeatherData } from '@/composables/useWeatherData'
 
-const locationStore = useLocationStore()
-const { location } = storeToRefs(locationStore)
-const { setLocation } = locationStore
-
-// 加载状态
-const loading = ref(true)
-
-const weather = ref<weatherInfo | null>(null)
-const uvIndex = ref<UVIndexInfo | null>(null)
-const sunrise = ref<string>('')
-const sunset = ref<string>('')
-const hourlyWeather = ref<HourlyWeatherInfo[]>([])
-const aqi = ref<AQIInfo | null>(null)
-const pollutants = ref<PollutantInfo[]>([])
-const airSources = ref<string[]>([])
-
-async function fetchAllData() {
-  loading.value = true
-  try {
-    const [weatherRes, uvRes, sunRes, hourlyWeatherRes, airRes] =
-      await Promise.all([
-        getWeatherApi(location.value.id),
-        getUVIndexApi(location.value.id),
-        getSunriseSunsetApi(location.value.id),
-        getHourlyWeatherApi(location.value.id),
-        getAirQualityApi(location.value.lat, location.value.lon)
-      ])
-    weather.value = weatherRes.now
-    uvIndex.value = uvRes.daily?.[0] || null
-    sunrise.value = sunRes.sunrise || ''
-    sunset.value = sunRes.sunset || ''
-    hourlyWeather.value = hourlyWeatherRes.hourly
-    aqi.value = airRes.indexes?.[0] || null
-    pollutants.value = airRes.pollutants || []
-    airSources.value = airRes.sources || []
-    formatHourlyWeather()
-  } finally {
-    loading.value = false
-  }
-}
-
-// 将当前天气转换为逐小时天气格式
-function toHourlyFormat(): HourlyWeatherInfo {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { obsTime, feelsLike, ...rest } = weather.value!
-  return {
-    fxTime: '现在',
-    ...rest
-  }
-}
-
-function formatHourlyWeather() {
-  hourlyWeather.value.unshift(toHourlyFormat())
-}
-
-async function handleCitySelect(city: CityInfo) {
-  setLocation(city)
-  await fetchAllData()
-}
-
-// 每 10 分钟自动刷新天气数据
-useIntervalFn(fetchAllData, 10 * 60 * 1000)
-
-onMounted(() => {
-  fetchAllData()
-})
+const {
+  location,
+  loading,
+  weather,
+  uvIndex,
+  sunrise,
+  sunset,
+  hourlyWeather,
+  aqi,
+  pollutants,
+  airSources,
+  handleCitySelect
+} = useWeatherData()
 </script>
 
 <template>
